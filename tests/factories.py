305a6fcc8a8bad8d5cdcdb569bdf7faf57e1ffdc
Ronan Takizawa <71115970+ronantakizawa@users.noreply.github.com>
2023-11-27 12:24:57 -0700

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -30,5 +30,37 @@ class ProductFactory(factory.Factory):
 
         model = Product
 
-    id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = factory.Faker("name")
+    description = factory.Faker("text")
+    price = FuzzyDecimal(100, 1000)
+    category = factory.SubFactory(CategoryFactory)
+
+    def create(self):
+        """Creates a fake product and returns it"""
+
+        product = Product(
+            name=self.name,
+            description=self.description,
+            price=self.price,
+            category=self.category,
+        )
+        product.save()
+        return product
+
+
+class CategoryFactory(factory.Factory):
+    """Creates fake categories for testing"""
+
+    class Meta:
+        """Maps factory to data model"""
+
+        model = Category
+
+    name = factory.Faker("name")
+
+    def create(self):
+        """Creates a fake category and returns it"""
+
+        category = Category(name=self.name)
+        category.save()
+        return category
